0d9d4c49c07f5d7c4a22d5a3691b12aae467afe9
shanasif1217 <108709785+shanasif1217@users.noreply.github.com>
2023-01-04 16:26:02 +0500

    Basic code
    A basic code to print Hello World in c++

---
+++ BASIC C++.cpp
@@ -0,0 +1,7 @@
+#include<iostream> //lib of c++
+using namespace std; //this is out syntax
+int main()
+{
+	cout << "Hello World!!!" << endl; //cout is used to get output on command prompt means to print on console.
+	return 0;
+}
